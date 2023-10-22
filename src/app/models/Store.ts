281009e82091933150e2ab20f073a383fbcafd32
Kaidong <kdnie20@gmail.com>
2023-10-22 15:00:42 -0400

    store.ts is no longer needed

--- src/app/models/Store.ts
+++
@@ -1,8 +0,0 @@
-import { Coord, TileState } from "./BoardState";
-
-export type BoardStateAction = 
-    { type: 'new-board', length: number, width: number, bombAmount: number } |
-    { type: 'tile-left-clicked', tileState: TileState, coord: Coord } |
-    { type: 'tile-right-clicked', tileState: TileState, coord: Coord }
-    ;
-
