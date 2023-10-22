f8429d8aad5113ed34ad91f206999c4f6d35c429
Kaidong <kdnie20@gmail.com>
2023-10-22 19:52:35 -0400

    starting with tiles

--- src/app/components/tile/tile.component.ts
+++ src/app/components/tile/tile.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { BoardState, Coord, TileState, initialTileState } from 'src/app/models/BoardState';
+import { TileMark } from 'src/app/models/TileDisplay';
+import { BoardActions } from 'src/app/store/actions/board.actions';
 
 @Component({
   selector: 'app-tile',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./tile.component.css']
 })
 export class TileComponent {
+  @Input() tileState: TileState;
+  @Input() coord: Coord;
 
+  face: string = '';
+
+  constructor(private store: Store<BoardState>) {
+    this.tileState = initialTileState;
+    this.coord = [-1, -1];
+  }
+
+  handleClick = (evt: MouseEvent) => {
+    if (evt.type === 'contextmenu')
+            evt.preventDefault();
+        
+    if(this.tileState.revealed) 
+      return;
+
+    if (evt.type === 'click' && this.tileState.mark == TileMark.Blank)
+      this.store.dispatch(BoardActions.tileLeftClicked({tile: this.tileState, coord: this.coord}));
+    else if (evt.type === 'contextmenu')            
+      this.store.dispatch(BoardActions.tileRightClicked({tile: this.tileState, coord: this.coord}));
+  }
 }
--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/models/AppState';
+import { BoardState } from 'src/app/models/BoardState';
+import { BoardActions } from 'src/app/store/actions/board.actions';
+import { selectTiles } from 'src/app/store/selectors/board.selectors';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
 
+  x: number = 0;
+  y: number = 0;
+  bombs: number = 0;
+
+  constructor(private store: Store<AppState>, private route: ActivatedRoute){
+  }
+
+  ngOnInit(): void {
+    this.x = this.route.snapshot.params['x'];
+    this.y = this.route.snapshot.params['y'];
+    this.bombs = this.route.snapshot.params['bombs'];
+
+    let props = {
+      xLength: this.x,
+      yLength: this.y,
+      bombAmount: this.bombs 
+    }
+
+    this.store.dispatch(BoardActions.generateNewBoard(props))
+  }
 }
